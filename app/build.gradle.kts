b6fb2d0fe9a8afe55652395c5896acfc9e22dd62
andreibarakovskii-spec <andreibarakovskii@gmail.com>
2026-08-21 08:57:06 +0300

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.imagine.livelingo"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.imagine.livelingo"
+        minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+        testInstrumentationRunner = "android.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("com.google.mlkit:translate:17.0.3")
+    implementation("com.google.mlkit:language-id:17.0.6")
+    testImplementation("junit:junit:4.13.2")
+}
